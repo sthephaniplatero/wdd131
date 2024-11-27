4d7bc0a9e726d23050f5a844a311f6dc222f9f31
sthephaniplatero <pla21002@byui.edu>
2024-11-27 16:18:43 -0600

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,6 @@
-// Obtener los elementos del menú
-const menuIcon = document.getElementById("menu-icon");
-const navMenu = document.getElementById("top-nav");
+const menuIcon = document.querySelector('.menu-icon');
+const topNav = document.querySelector('.top-nav');
 
-// Agregar evento de clic al ícono de menú
-menuIcon.addEventListener("click", () => {
-    // Alternar la clase "active" para mostrar/ocultar el menú
-    navMenu.classList.toggle("active");
+menuIcon.addEventListener('click', () => {
+    topNav.classList.toggle('active'); // Muestra u oculta el menú
 });

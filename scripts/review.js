2210d52a04596060ba62bcac3a0f2d532bd553bd
sthephaniplatero <pla21002@byui.edu>
2024-12-11 12:21:42 -0600

    update

--- scripts/review.js
+++ scripts/review.js
@@ -1,18 +1,17 @@
 const reviewsKey = "reviewCount";
 
-// Función para incrementar y mostrar el contador
+
 function updateReviewCount() {
-    // Recupera el valor actual del contador desde localStorage, o inicializa en 0
+   
     let reviewCount = parseInt(localStorage.getItem(reviewsKey)) || 0;
 
-    // Actualiza el contador mostrado en la página
+   
     const reviewCountElement = document.querySelector(".reviews-count");
     if (reviewCountElement) {
         reviewCountElement.textContent = reviewCount;
     }
 }
 
-// Función para restablecer el contador a cero
 function resetReviewCount() {
     localStorage.setItem(reviewsKey, 0);
     const reviewCountElement = document.querySelector(".reviews-count");
@@ -21,5 +20,4 @@ function resetReviewCount() {
     }
 }
 
-// Ejecuta la función al cargar la página para mostrar el contador actual
 document.addEventListener("DOMContentLoaded", updateReviewCount);
